c0a54c8d2d43fd53ec9b54c20c4215a62b70bff3
danila_goncharov <danniila2000@mail.ru>
2024-03-25 17:16:48 +0300

    реализована админка для модели products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+        list_display - отвечает за отображение полей в таблице
+        list_display_links - отвечает за то какое поле является ссылкой
+        ordering - отвечает за сортировку
+        search_fields - по каким полям осуществляется поиск
+
+
+    """
+    #
+    list_display = 'pk', 'name', 'description_short', 'price'
+    list_display_links = 'pk', 'name'
+    ordering = 'pk',
+    search_fields = 'name',
+    change_list_template = 'accounts/admin/profile_change_list.html'
+
+    def description_short(self, obj: Product) -> str:
+        if len(obj.description) > 30:
+            return obj.description[:30] + '...'
+        else:
+            return obj.description
+
--- products/models.py
+++ products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField(max_length=200, null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f'Product(pk={self.pk}, name={self.name!r})'
